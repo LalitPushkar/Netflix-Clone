cdbce6bb0815df29fe392779228f2ea5d5b97cf1
LalitPushkar <lalitpushkar03@gmail.com>
2023-09-12 17:32:42 +0530

    New project on netflix clone

---
+++ app.js
@@ -0,0 +1,5 @@
+let first = 10;
+let second = 5;
+//console.log("The sum of these numbers are :", first + second , "only");
+let sum = `The sum of number is : ${first + second} only`;
+console.log(sum);
